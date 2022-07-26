c3f3574454a36aa4e7710c25d3934c1f7af560db
katooo-jp <katooo.py@gmail.com>
2022-07-26 16:32:03 +0900

    Remove: Django dir.

--- Django/sns_app/backend/sns/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Users, Posts,Photos, Comments, Likes
-
-# SNS_DB
-admin.site.register(Users)
-admin.site.register(Photos)
-admin.site.register(Posts)
-admin.site.register(Comments)
-admin.site.register(Likes)
